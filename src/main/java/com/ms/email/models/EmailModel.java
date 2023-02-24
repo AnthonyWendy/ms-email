e9677008d63c28c056791b74b5b59a1a30d19362
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2023-02-23 21:29:34 -0300

    Método de Post

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -1,2 +1,30 @@
-package com.ms.email.models;public class EmailModel {
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "email")
+public class EmailModel implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDattEmail;
+    private StatusEmail statusEmail;
+
+
 }
